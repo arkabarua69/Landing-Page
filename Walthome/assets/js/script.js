ee3b86125972f3f1f86389d47d199fbf3c4d82a2
arkabarua69 <arkaturna@gmail.com>
2025-02-16 21:38:52 +0600

    Walthome

---
+++ Walthome/assets/js/script.js
@@ -0,0 +1,24 @@
+'use strict'; 
+/** 
+navbar toggle in mobile 
+*/
+
+const /** [NodeElement] */ $navbar = document.querySelector("[data-navbar]"); 
+const /* {NodeElement} */ $navToggler = document.querySelector("[data-nav-toggler]");
+const /* {NodeElement} */ $header = document.querySelector("[data-header]");
+
+$navToggler.addEventListener("click", () => $navbar.classList.toggle("active")); 
+
+// Header scroll state 
+
+window.addEventListener("scroll", e => { 
+    $header.classList[window.scrollY > 50 ? "add" : "remove"]("active"); 
+});
+
+// Add to favorite button toggle 
+const /** (NodeList) */ $toggleBtns = document.querySelectorAll("[data-toggle-btn]"); 
+$toggleBtns.forEach($toggleBtn => { 
+    $toggleBtn.addEventListener("click", () => { 
+        $toggleBtn.classList.toggle("active"); 
+    }); 
+});
---
+++ Xora/src/App.jsx
@@ -0,0 +1,13 @@
+import Headers from "./sections/Header.jsx";
+import Hero from "./sections/Hero.jsx";
+
+const App = () => {
+  return (
+    <main className="overflow-hidden">
+      <Headers />
+      <Hero />
+    </main>
+  );
+};
+
+export default App;
